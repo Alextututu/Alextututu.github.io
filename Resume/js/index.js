ca6fc343dd3d132f897e1a4ed0d49a6eb16d4fb7
Alextututu <969289239@qq.com>
2018-03-13 15:00:24 +0800

    个人简历

--- Resume/js/index.js
+++ Resume/js/index.js
@@ -19,21 +19,58 @@ $(function(){
                 (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));              // firefox
 		    if (delta > 0) {
 		        // 向上滚
-		        
-//		        $("#box").stop().animate({top:$(window).height()*num+"px"},500);
-				move("up",num);
+				move("up");
 		    } else if (delta < 0) {
 		        // 向下滚
-		        
 		        move("down");
-//		        	boeal=true;
-//			        if(boeal==true){
-//			        	num<4?num++:num=4;
-//			        	$("#box").stop().animate({top:"-"+$(window).height()*num+"px"},500);
-//			        	boeal=false;
-//			        }
 		   }   
 	});
+	//当手指触摸到屏幕的时候触发
+	$("body").on("touchstart", function(e) {
+    // 判断默认行为是否可以被禁用
+    if (e.cancelable) {
+        // 判断默认行为是否已经被禁用
+        if (!e.defaultPrevented) {
+            e.preventDefault();
+        }
+    }   
+    startX = e.originalEvent.changedTouches[0].pageX,
+    startY = e.originalEvent.changedTouches[0].pageY;
+});
+$("body").on("touchend", function(e) {         
+    // 判断默认行为是否可以被禁用
+    if (e.cancelable) {
+        // 判断默认行为是否已经被禁用
+        if (!e.defaultPrevented) {
+            e.preventDefault();
+        }
+    }               
+    moveEndX = e.originalEvent.changedTouches[0].pageX,
+    moveEndY = e.originalEvent.changedTouches[0].pageY,
+    X = moveEndX - startX,
+    Y = moveEndY - startY;
+    //左滑
+    if ( X > 0 ) {
+     move("up");
+    }
+    //右滑
+    else if ( X < 0 ) {
+        move("down");
+    }
+    //下滑
+    else if ( Y > 0) {
+       move("down");  
+    }
+    //上滑
+    else if ( Y < 0 ) {
+        move("up");
+    }
+    //单击
+    else{
+        alert('单击');    
+    }
+});
+	//移动方法
 	function move(direction){
 		console.log($(this));
 		 var $this = $(this);
